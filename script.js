8ea8d7eeab88c95f98b7852411f6237c2add662d
sweta singh <89792379+swetasingh03@users.noreply.github.com>
2021-10-20 13:43:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var red=document.getElementById('red')
+red.addEventListener('mouseover',function(){
+    document.body.style.backgroundColor="#ff0000";
+})
+var blue=document.getElementById('blue')
+blue.addEventListener('mouseover',function(){
+    document.body.style.backgroundColor="#0000ff";
+})
+var green=document.getElementById('green')
+green.addEventListener('mouseover',function(){
+    document.body.style.backgroundColor="#00ff00";
+})
+var yellow=document.getElementById('yellow')
+yellow.addEventListener('mouseover',function(){
+    document.body.style.backgroundColor="#ffff00";
+})
+var pink=document.getElementById('pink')
+pink.addEventListener('mouseover',function(){
+    document.body.style.backgroundColor="#ff00bf";
+})
